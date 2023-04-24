2fa04ba74198d9d9b9a3c27b2f4fb0931550b8d4
Ducky <likavecpeter@gmail.com>
2023-04-24 14:03:17 +0200

    Product init

---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/product/logic/ProductService.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.oop.assignment3.product.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sk.stuba.fei.uim.oop.assignment3.product.data.IProductRepository;
+
+
+@Service
+public class ProductService implements IProductService{
+    @Autowired
+    private IProductRepository repository;
+}
